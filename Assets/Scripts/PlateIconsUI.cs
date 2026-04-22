123d27bb157113cb3a834e7bc3906c3351c19016
ZerolesLuka <luka.lekarczyk28@gmail.com>
2026-04-22 17:57:30 -0400

    Plate World UI Icons #2
    Finished the plate world UI icon lesson. Basically, we created an event that happens when an ingredient is valid and added to thee plate. On this event, we do a foreach to see what ingredient it is, and simply set active the ui icon according to thee ingredient.

--- Assets/Scripts/PlateIconsUI.cs
+++ Assets/Scripts/PlateIconsUI.cs
@@ -3,24 +3,37 @@ using UnityEngine;
 public class PlateIconsUI : MonoBehaviour
 {
     [SerializeField] private PlateKitchenObject plateKitchenObject; //reference to the plate kitchen object 
-    [SerializeField] private Transform iconTemplate;
+    [SerializeField] private Transform iconTemplate; //reference to the icon template
 
+
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false); //hide the icon template
+    }
 
     private void Start()
     {
-        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded; //sub to event
     }
 
-    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e) //event wheen an ingredient is added to the plate
     {
-        
+        UpdateVisual(); //update the visual of the plate icons
     }
 
     private void UpdateVisual()
     {
+        foreach(Transform child in transform)
+        {
+            if (child == iconTemplate) continue; //skip the icon template
+            Destroy(child.gameObject);
+        }
+        
         foreach(KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
         {
-
+            Transform iconTransform = Instantiate(iconTemplate, transform);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
         }
     }
 }
