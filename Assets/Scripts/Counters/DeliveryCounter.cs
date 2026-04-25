ae4f06fdaf9a47826de6b62fb3a214921cac3255
ZerolesLuka <luka.lekarczyk28@gmail.com>
2026-04-25 13:53:30 -0400

    Delivery Counter, Shader Graph
    In todays lecture I focused a lot more on animation then code. Basically for today I learned about shader graph, which is a visual alternative to coding your own shaders. I learned how to build custom and moving sprites. How to make them properly transparent, how to create shader variants within the same graph, and overall I just played around with it. I am glad that this is now in my toolbox, and I will move on to the next lecture.

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject()){ //if something
+            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))//if it's a plate
+            { 
+                player.GetKitchenObject().DestroySelf(); //destroy the plate on player
+            }
+            
+        }
+    }
+}
